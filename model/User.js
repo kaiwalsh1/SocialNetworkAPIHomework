944e03575e4c432b68dd5cba435713f873501863
kai <kai@kaiconsulting.co>
2022-03-02 11:49:00 -0700

    updated User model

--- model/User.js
+++ model/User.js
@@ -4,7 +4,29 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema({
     username: {
         type: String,
+        unique: true,
+        required: true,
         trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(value) {
+                return isEmail(value);
+            },
+            message: function(userObject) {
+                return `${userObject.email} is not a valid email address`;
+            },
+        },
+    },
+    thoughts: {
+        type: String,
+
+    },
+    friends: {
+        type: String,
     },
 });
 
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
+const User = require('./model/User');
 
 mongoose.connect('mongodb://localhost:27017/socialNetworkDB')
     .then(() => {
         console.log('Successfully connected to MongoDB');
     })
     .catch(err => console.log(err));
-
-    
